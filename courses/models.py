8ce6d24b71a01cd48293585a7866ac063d3da637
Juwon Hwang <wndnjs719@gmail.com>
2023-08-28 10:04:08 +0900

    Initial commit

---
+++ courses/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Subject(models.Model):
+    pass
+
+
+class Course(models.Model):
+    pass
+
+
+class Module(models.Model):
+    pass
