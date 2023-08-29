47493b46768868886cbd0b1e9bd2529b876d7fac
Juwon Hwang <wndnjs719@gmail.com>
2023-08-29 11:03:21 +0900

    url 정의

--- educa/urls.py
+++ educa/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
@@ -26,6 +26,7 @@ urlpatterns = [
     path('accounts/logout/', auth_views.LogoutView.as_view(),
          name='logout'),
     path('admin/', admin.site.urls),
+    path('course/', include('courses.urls'))
 ]
 
 if settings.DEBUG:
