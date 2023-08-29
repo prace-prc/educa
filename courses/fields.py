b27b526d7ab64b4a90d66511692624d5f859f351
Juwon Hwang <wndnjs719@gmail.com>
2023-08-29 14:12:31 +0900

    들여쓰기 안되어있던 오류 수정

--- courses/fields.py
+++ courses/fields.py
@@ -7,19 +7,18 @@ class OrderField(models.PositiveIntegerField):
         self.for_fields = for_fields
         super().__init__(*args, **kwargs)
 
-
-def pre_save(self, model_instance, add):
-    if getattr(model_instance, self.attname) is None:
-        try:
-            qs = self.model.objects.all()
-            if self.for_fields:
-                query = {field: getattr(model_instance, field) for field in self.for_fields}
-                qs = qs.filter(**query)
-            last_item = qs.latest(self.attname)
-            value = last_item.order + 1
-        except ObjectDoesNotExist:
-            value = 0
-        setattr(model_instance, self.attname, value)
-        return value
-    else:
-        return super().pre_save(model_instance, add)
+    def pre_save(self, model_instance, add):
+        if getattr(model_instance, self.attname) is None:
+            try:
+                qs = self.model.objects.all()
+                if self.for_fields:
+                    query = {field: getattr(model_instance, field) for field in self.for_fields}
+                    qs = qs.filter(**query)
+                last_item = qs.latest(self.attname)
+                value = last_item.order + 1
+            except ObjectDoesNotExist:
+                value = 0
+            setattr(model_instance, self.attname, value)
+            return value
+        else:
+            return super().pre_save(model_instance, add)
