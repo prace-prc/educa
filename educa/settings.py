70c4d3a92dce8d75a0374d39bcff9b752e027501
Juwon Hwang <wndnjs719@gmail.com>
2023-08-31 11:05:55 +0900

    파일, 텍스트, 이미지, 영상 렌더링 위한 템플릿과 영상을 위한 django-embed-video 패키지 설치 후 설정에 추가

--- educa/settings.py
+++ educa/settings.py
@@ -35,6 +35,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'courses.apps.CoursesConfig',
     'students.apps.StudentsConfig',
+    'embed_video',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
