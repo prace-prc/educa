c8f2303ebd0c6402b6f468b348700690938ccd9e
Juwon Hwang <wndnjs719@gmail.com>
2023-09-04 13:05:20 +0900

    뷰셋 및 라우터 생성

--- courses/api/views.py
+++ courses/api/views.py
@@ -1,11 +1,11 @@
-from rest_framework import generics
+from rest_framework import generics, viewsets
 from rest_framework.authentication import BasicAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
 from courses.models import Subject, Course
-from courses.api.serializers import SubjectSerializer
+from courses.api.serializers import SubjectSerializer, CourseSerializer
 
 
 class SubjectListView(generics.ListAPIView):
@@ -26,3 +26,7 @@ class CourseEnrollView(APIView):
         course = get_object_or_404(Course, pk=pk)
         course.students.add(request.user)
         return Response({'enrolled': True})
+
+class CourseViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
