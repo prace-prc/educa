6c7b5a0c89b5a3a00949580ba517493cb953bc5d
Juwon Hwang <wndnjs719@gmail.com>
2023-09-05 11:13:32 +0900

    채팅 룸 뷰 작성

--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseForbidden
 from django.shortcuts import render
 
+
 # Create your views here.
+@login_required
+def course_chat_room(request, course_id):
+    try:
+        course = request.user.courses_joined.get(id=course_id)
+    except:
+        return HttpResponseForbidden()
+    return render(request, 'chat/room.html', {'course': course})
